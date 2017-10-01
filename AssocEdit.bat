8c1716e7fb319012e21f49cbcf888a7e92e81190
TheJoshua974 <joshuavandaele@gmail.com>
2017-10-01 21:13:14 +0200

    1.0

---
+++ AssocEdit.bat
@@ -0,0 +1,9 @@
+@echo off
+echo.Hello %username! What file type do you wanna change the program association?
+set /p ext=
+echo.What do you want the file type to be? (leave empty to remove file association)
+set /p filetype=
+echo.What do you want the .%ext% files to be executed with? (program path) (leave empty to remove file association)
+set /p program=
+assoc ."%ext%"="%filetype%"
+ftype "%filetype%"="%program%" %1
